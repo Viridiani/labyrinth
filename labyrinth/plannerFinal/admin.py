e376c514dae8c8efb9eb1a1a5c09526688be38de
Avery <averychristie50@gmail.com>
2023-08-27 23:14:38 -0600

    Added admin to plannerFinal and *finished* tables
    Need to migrate and still double-checking if we got all the categories needed. Plus, need to add __str__ method.

--- labyrinth/plannerFinal/admin.py
+++ labyrinth/plannerFinal/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Domains, Entities
 
-# Register your models here.
+admin.site.register(Domains)
+admin.site.register(Entities)
--- labyrinth/plannerFinal/models.py
+++ labyrinth/plannerFinal/models.py
@@ -5,6 +5,7 @@ class Domains(models.Model):
     domain_name = models.CharField(max_length=100)
     start_date = models.DateField("Beginning Date")
     end_date = models.DateField("Ending Date")
+    description = models.CharField(max_length=300)
 
 
 
@@ -13,10 +14,8 @@ class Entities(models.Model):
     entity_name = models.CharField(max_length=100)
     start_date_time = models.DateTimeField("Entry Date")
     end_date_time = models.DateTimeField("Due Date")
+    text = models.TextField("Notes")
 
 
-# need to add admin login as well?? probably not
 # Check this: https://www.techwithtim.net/tutorials/django/sqlite3-database
-# Because to some extent, Events/Organizations and ToDoList/Items should be the same
-# Need to check that in the planner (not plannerFinal) app
-# This will actually need to be sketched out for database planning
+# Need to check above in the planner (not plannerFinal) app
